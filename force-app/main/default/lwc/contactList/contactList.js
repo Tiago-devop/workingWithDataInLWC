94ffceaa42d35bf657608c6fdd1aa12551d20cf0
Tiago <tiagodev@protonmail.com>
2022-11-07 13:00:36 -0300

    throwing errors

--- force-app/main/default/lwc/contactList/contactList.js
+++ force-app/main/default/lwc/contactList/contactList.js
@@ -3,6 +3,7 @@ import FIRST_NAME_FIELD from '@salesforce/schema/Contact.FirstName';
 import LAST_NAME_FIELD from '@salesforce/schema/Contact.LastName';
 import EMAIL_FIELD from '@salesforce/schema/Contact.Email';
 import getContacts from '@salesforce/apex/ContactController.getContacts';
+import { reduceErrors } from 'c/ldsUtils';
 
 const COLUMNS = [
     {label: 'Contact First Name', fieldName: FIRST_NAME_FIELD.fieldApiName, type: 'text'},
@@ -14,4 +15,8 @@ export default class ContactList extends LightningElement {
     columns = COLUMNS;
     @wire(getContacts)
     contacts;
+
+    get errors() {
+        return (this.contacts.error) ? reduceErrors(this.contacts.error) : [];
+    }
 }
